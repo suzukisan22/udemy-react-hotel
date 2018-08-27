7fc1c8d428f7d7808206140ac8a2a20c51483d0d
suzuki.r <getty2017@gmail.com>
2018-08-27 23:00:55 +0900

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
+import Map from './Map';
+
+import { geocode } from '../domain/Geocoder';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {
+        lat: 35.6585805,
+        lng: 139.7454329,
+      },
+    };
+  };
+
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      location: {
+        lng: 0,
+        lat: 0,
+      },
+    });
+  }
+
+  handlePlaceSubmit(place) {
+    geocode(place)
+      .then(({ status, address, location }) => {
+        switch (status) {
+          case 'OK': {
+            this.setState({ address, location });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage("結果がみつかりませんでした。");
+            break;
+          }
+          default: {
+            this.setErrorMessage("エラーが発生しました。");
+          }
+        }
+      })
+      .catch(() => {
+        this.setErrorMessage("通信に失敗しました。");
+      });
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <GeocodeResult
+          address={this.state.address}
+          location={this.state.location}
+        />
+        <Map location={this.state.location} />
+      </div>
+    );
+  }
+}
+
+export default App;
